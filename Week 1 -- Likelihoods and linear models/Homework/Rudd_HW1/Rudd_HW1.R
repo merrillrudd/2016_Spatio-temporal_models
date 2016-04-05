9ddfaefec61d0dfa0cb08f0865486f944a88d459
merrillrudd <merrillrudd@gmail.com>
2016-04-04 17:10:15 -0700

    part 1.1 hw 1

---
+++ Week 1 -- Likelihoods and linear models/Homework/Rudd_HW1/Rudd_HW1.R
@@ -0,0 +1,30 @@
+rm(list=ls())
+setwd("C:\\Git_Projects\\2016_Spatio-temporal_models\\Week 1 -- Likelihoods and linear models\\Homework\\Rudd_HW1")
+
+# devtools::install_github("nwfsc-assess/geostatistical_delta-GLMM")
+library( SpatialDeltaGLMM )
+data( EBS_pollock_data )
+library(TMB)
+
+## compile template file
+compile( "glm_hw.cpp" )
+
+## data
+catch <- EBS_pollock_data$catch
+
+## design matrix
+X_list <- NULL
+X_list[[1]] <- as.matrix(rep(1, length(catch)))
+
+dyn.load( dynlib("glm_hw") )
+Data <- list(model=1, "y_i"=catch, "X_ij"=as.matrix(X_list[[1]]))
+Parameters <- list("b_j"=rep(0,ncol(X_list[[1]])), "theta_z"=c(0,0))
+Obj <- MakeADFun( data=Data, parameters=Parameters)
+
+Opt <- nlminb( start=Obj$par, objective=Obj$fn, gradient=Obj$gr )
+Opt$diagnostics <- data.frame( "name"=names(Obj$par), "Est"=Opt$par, 
+	"final_gradient"=as.vector(Obj$gr(Opt$par)) )
+
+SD <- sdreport( Obj )
+
+Report = Obj$report()
