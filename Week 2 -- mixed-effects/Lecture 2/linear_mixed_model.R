f7b8e1389600cd0a921a7ca0e0f7e5c1bd183249
merrillrudd <merrillrudd@gmail.com>
2016-04-05 10:03:37 -0700

    my edits to lecture code

--- Week 2 -- mixed-effects/Lecture 2/linear_mixed_model.R
+++ Week 2 -- mixed-effects/Lecture 2/linear_mixed_model.R
@@ -1,6 +1,6 @@
-
-setwd( "C:/Users/James.Thorson/Desktop/Project_git/2016_Spatio-temporal_models/Week 2 -- mixed-effects/Lecture 2" )
-Use_REML = TRUE
+rm(list=ls())
+setwd("C:\\Git_Projects\\2016_Spatio-temporal_models\\Week 2 -- mixed-effects\\Lecture 2")
+Use_REML = FALSE
 
 ######################
 # Simulate data
