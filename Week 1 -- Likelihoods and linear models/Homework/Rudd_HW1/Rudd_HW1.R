0232971405d3d40eeda22207dcf4fc84fdb63aeb
merrillrudd <merrillrudd@gmail.com>
2016-04-06 16:04:56 -0700

    homework 1 submitted

--- Week 1 -- Likelihoods and linear models/Homework/Rudd_HW1/Rudd_HW1.R
+++ Week 1 -- Likelihoods and linear models/Homework/Rudd_HW1/Rudd_HW1.R
@@ -5,6 +5,7 @@ setwd("C:\\Git_Projects\\2016_Spatio-temporal_models\\Week 1 -- Likelihoods and 
 library( SpatialDeltaGLMM )
 data( EBS_pollock_data )
 library(TMB)
+library(beanplot)
 
 ## compile template file
 compile( "glm_hw.cpp" )
@@ -88,7 +89,7 @@ X[[2]] <- as.matrix(cbind(rep(1,length(catch)), EBS_pollock_data$lat, EBS_polloc
 Data <- NULL
 Data[[1]] <- list("like"=1, "y_i"=catch, "X_ij"=as.matrix(X[[1]]), "include"=rep(0,length(catch))) # delta lognormal
 Data[[2]] <- list("like"=2, "y_i"=catch, "X_ij"=as.matrix(X[[1]]), "include"=rep(0,length(catch))) # delta gamma
-Data[[3]] <- list("like"=2, "y_i"=catch, "X_ij"=as.matrix(X[[2]]), "include"=rep(0,length(catch))) # delta lognormal with lat/lon covariate
+Data[[3]] <- list("like"=2, "y_i"=catch, "X_ij"=as.matrix(X[[2]]), "include"=rep(0,length(catch))) # delta gamma with lat/lon covariate
 
 
 Parameters <- NULL
@@ -104,7 +105,7 @@ Opt1 <- model1$Opt
 Like1 <- model1$Opt$objective
 npar1 <- nrow(model1$Opt$diagnostic)
 set.seed(1234)
-pprob1 <- crossValidate(K=10, data=Data[[1]], parameters=Parameters[[1]])
+cvscore1 <- crossValidate(K=10, data=Data[[1]], parameters=Parameters[[1]])
 
 ## model 2 - delta gamma
 model2 <- runModel(data=Data[[2]], parameters=Parameters[[2]])
@@ -113,36 +114,37 @@ Opt2 <- model2$Opt
 Like2 <- model2$Opt$objective
 npar2 <- nrow(model2$Opt$diagnostic)
 set.seed(1234)
-pprob2 <- crossValidate(K=10, data=Data[[2]], parameters=Parameters[[2]])
+cvscore2 <- crossValidate(K=10, data=Data[[2]], parameters=Parameters[[2]])
 
 
-## model 3 - negative binomial
+## model 3 - delta gamma with covariates
 model3 <- runModel(data=Data[[3]], parameters=Parameters[[3]])
 Report3 <- model3$Report
 Opt3 <- model3$Opt
 Like3 <- model3$Opt$objective
 npar3 <- nrow(model3$Opt$diagnostic)
 set.seed(1234)
-pprob3 <- crossValidate(K=10, data=Data[[3]], parameters=Parameters[[3]])
+cvscore3 <- crossValidate(K=10, data=Data[[3]], parameters=Parameters[[3]])
 
 
 
 ## plot model fits
-par(mfrow=c(2,2))
-hist(log(1+catch), freq=FALSE, col=rgb(1,0,0,0.2))
+par(mfrow=c(3,1))
+hist(log(1+catch), freq=FALSE, col=rgb(1,0,0,0.2), main="Delta-lognormal", xlab="log(1+catch)")
 set.seed(123)
 Sim_catch1 <- simulateData(like=1, report=Report1)
 hist(log(1+Sim_catch1), freq=FALSE, add=TRUE, col=rgb(0,0,1,0.2))
 
-hist(log(1+catch), freq=FALSE, col=rgb(1,0,0,0.2))
+hist(log(1+catch), freq=FALSE, col=rgb(1,0,0,0.2), main="Delta-gamma", xlab="log(1+catch)")
 set.seed(123)
 Sim_catch2 <- simulateData(like=2, report=Report2)
 hist(log(1+Sim_catch2), freq=FALSE, add=TRUE, col=rgb(0,1,0,0.2))
 
-hist(log(1+catch), freq=FALSE, col=rgb(1,0,0,0.2))
+hist(log(1+catch), freq=FALSE, col=rgb(1,0,0,0.2), main="Delta-gamma with covar", xlab="log(1+catch)")
 set.seed(123)
 Sim_catch3 <- simulateData(like=2, report=Report3)
 hist(log(1+Sim_catch3), freq=FALSE, add=TRUE, col=rgb(1,1,0,0.2))
+
 
 ##########################################
 ### Part 2 - simulation experiment
@@ -186,3 +188,48 @@ for(ss in sim_vec){
     }
 }
 end <- Sys.time() - start
+
+## plot relative error
+par(mfrow=c(3,1), mar=c(0,0,0,0), omi=c(1,1,1,2))
+# ylim1 <- c(-5,5)
+ylim1 <- c(-3,3)
+beanplot(x=1:3, as.data.frame(relerr[,,1]), what=c(0,1,1,0), xlim=c(1.5,4.5), ylim=ylim1,
+    cex=0.5, beanlinewd=3, maxstripline=0.5, maxwidth=0.75, xaxt="n",
+    log="",overalline="median", beanlines="median", col=rgb(1,0,0,1), xlab="", ylab="", xaxs="i", yaxs="i",
+    na.rm=TRUE, border=NA, na.rm=TRUE)
+polygon(x=c(1,2.5,2.5,1), y=c(-5,-5,5,5), border=NA, col=rgb(1,0,0,0.2))
+polygon(x=c(2.5,3.5,3.5,2.5), y=c(-5,-5,5,5), border=NA, col=rgb(0,1,1,0.2))
+polygon(x=c(3.5,4.5,4.5,3.5), y=c(-5,-5,5,5), border=NA, col=rgb(0,0,1,0.2))
+abline(h=0, lty=2)
+mtext(side=4, "Delta-lognormal", line=2)
+
+ylim2 <- c(-3,3)
+beanplot(x=1:3, as.data.frame(relerr[,,2]), what=c(0,1,1,0), xlim=c(1.5,4.5), ylim=ylim2,
+    cex=0.5, beanlinewd=3, maxstripline=0.5, maxwidth=0.75, xaxt="n",
+    log="",overalline="median", beanlines="median", col=rgb(0,1,1,1), xlab="", ylab="", xaxs="i", yaxs="i",
+    na.rm=TRUE, border=NA, na.rm=TRUE)
+abline(h=0, lty=2)
+polygon(x=c(1,2.5,2.5,1), y=c(-5,-5,5,5), border=NA, col=rgb(1,0,0,0.2))
+polygon(x=c(2.5,3.5,3.5,2.5), y=c(-5,-5,5,5), border=NA, col=rgb(0,1,1,0.2))
+polygon(x=c(3.5,4.5,4.5,3.5), y=c(-5,-5,5,5), border=NA, col=rgb(0,0,1,0.2))
+mtext(side=4, "Delta-gamma", line=2)
+
+# ylim3 <- c(-3, 3.5)
+ylim3 <- c(-3,3)
+beanplot(x=1:3, as.data.frame(relerr[,,3]), what=c(0,1,1,0), xlim=c(1.5,4.5), ylim=ylim3,
+    cex=0.5, beanlinewd=3, maxstripline=0.5, maxwidth=0.75, xaxt="n",
+    log="",overalline="median", beanlines="median", col=rgb(0,0,1,1), xlab="", ylab="", xaxs="i", yaxs="i",
+    na.rm=TRUE, border=NA, na.rm=TRUE)
+abline(h=0, lty=2)
+polygon(x=c(1,2.5,2.5,1), y=c(-5,-5,5,5), border=NA, col=rgb(1,0,0,0.2))
+polygon(x=c(2.5,3.5,3.5,2.5), y=c(-5,-5,5,5), border=NA, col=rgb(0,1,1,0.2))
+polygon(x=c(3.5,4.5,4.5,3.5), y=c(-5,-5,5,5), border=NA, col=rgb(0,0,1,0.2))
+
+mtext(side=4, "Delta-gamma with covar", line=2)
+axis(1, 2:4, c("Delta-lognormal", "Delta-gamma", "Delta-gamma with covar"), cex.axis=1.5)
+mtext(side=1, "Estimation Model", outer=TRUE, line=3, cex=1.5)
+mtext(side=4, "Simulation Model", outer=TRUE, line=4, cex=1.5)
+mtext(side=2, "Relative Error", outer=TRUE, line=4, cex=1.5)
+            
+
+
