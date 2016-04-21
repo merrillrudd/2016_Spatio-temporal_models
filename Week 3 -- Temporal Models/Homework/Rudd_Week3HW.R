101d29a6fef6472bd3ae658090ee48627fb0de52
merrillrudd <merrillrudd@gmail.com>
2016-04-21 09:15:21 -0700

    homework

--- Week 3 -- Temporal Models/Homework/Rudd_Week3HW.R
+++ Week 3 -- Temporal Models/Homework/Rudd_Week3HW.R
@@ -14,18 +14,12 @@ library(TMB)
 compile("dlm_hw3.cpp")
 dyn.load(dynlib("dlm_hw3"))
 
-ar <- c(-0.5, 0, 0.5)
-proc <- 0.4
-obs <- c(0.5*proc, proc, 2*proc)
-iters <- 100
-
 ###########################
 ## functions
 ###########################
 
 DLM_sim <- function(N = 100, seed, sigma.obs = 1, sigma.proc = 0.5, a = -0.5, y1 = 10){
     set.seed(seed)
-    N <- 100
     ytrue <- numeric(length = N)
     ytrue[1] <- y1
     log.sigma.proc <- log(sigma.proc)
@@ -34,18 +28,22 @@ DLM_sim <- function(N = 100, seed, sigma.obs = 1, sigma.proc = 0.5, a = -0.5, y1
     for(i in 2:N) {
         ytrue[i] <- a*ytrue[i-1] + proc.error[i-1]
     }
-      x <- seq_len(N)
       yobs <- rnorm(N, mean = ytrue, sd = sigma.obs)
     return(list(yobs = yobs, ytrue = ytrue) )
 }
 
-DLM_run <- function(sim){
+DLM_run <- function(sim, model){
 		
 		#Run TMB model on simulated data
 	        N = length(sim$yobs)	
-	        data <- list(y = sim$yobs)
-	        parameters <- list(a = 0.5, log_sigma_proc = -1, log_sigma_obs = -1, u = rep(mean(sim$yobs), N))
-	        obj <- MakeADFun(data, parameters, random = "u", DLL = "dlm_hw3")
+	        data <- list(model=model, y = sim$yobs)
+	        parameters <- list(a = 0.5, b=0.5, log_sigma_proc = -1, log_sigma_obs = -1, u = rep(mean(sim$yobs), N))
+	        if(model==0){
+	        	map <- list()
+	        	map[["b"]] <- factor(NA)
+	        	obj <- MakeADFun(data, parameters, random = "u", map, DLL = "dlm_hw3")
+	        }
+	        if(model==1) obj <- MakeADFun(data, parameters, random = "u", DLL = "dlm_hw3")
 	        obj$hessian <- FALSE
 	        opt <- do.call("optim", obj)
 	        sd <- sdreport(obj)	
@@ -53,15 +51,34 @@ DLM_run <- function(sim){
 	        # extract fixed effects:
 	  		fixed <- summary(sd, "fixed")
 
-	Outs <- data.frame("a_est"=fixed["a","Estimate"], "proc_est"=exp(fixed["log_sigma_proc", "Estimate"])^2, "obs_est"=exp(fixed["log_sigma_obs", "Estimate"])^2, stringsAsFactors=FALSE)
+	if(model==0) Outs <- data.frame("a_est"=fixed["a","Estimate"], "proc_est"=exp(fixed["log_sigma_proc", "Estimate"])^2, "obs_est"=exp(fixed["log_sigma_obs", "Estimate"])^2, stringsAsFactors=FALSE)
+	if(model==1) Outs <- summary(sd)[which(rownames(summary(sd)) %in% c("a", "b", "sigma_proc", "sigma_obs")),]
 	return(Outs)
+}
+
+gompertz_sim <- function(N = 100, seed, sigma.obs, sigma.proc, a, b, y1){
+	set.seed(seed)
+    ytrue <- numeric(length = N)
+    ytrue[1] <- y1
+    log.sigma.proc <- log(sigma.proc)
+    proc.error <- rnorm(N, mean = 0, sd = sigma.proc)
+    log.sigma.obs <- log(sigma.obs)
+    for(i in 2:N) {
+        ytrue[i] <- a + b*ytrue[i-1] + proc.error[i-1]
+    }
+      yobs <- rnorm(N, mean = ytrue, sd = sigma.obs)
+    return(list(yobs = yobs, ytrue = ytrue) )
 }
 
 
 
 ###########################
-## simulation
+## simulation experiment
 ###########################
+ar <- c(-0.5, 0, 0.5)
+proc <- 0.4
+obs <- c(0.5*proc, proc, 2*proc)
+iters <- 100
 
 ## setupt output
 a_output <- list()
@@ -79,7 +96,7 @@ for(aa in 1:length(ar)){
 	for(oo in 1:length(obs)){
 
 		sim_list <- lapply(1:iters, function(x) DLM_sim(a=ar[aa], seed=x, N=100, sigma.obs=obs[oo], sigma.proc=0.4, y1=3))
-		res_list <- sapply(1:iters, function(x) DLM_run(sim=sim_list[[x]]))
+		res_list <- sapply(1:iters, function(x) DLM_run(sim=sim_list[[x]], model=0))
 
 		a_output[["mean"]][aa,oo] <- mean(unlist(res_list["a_est",]))
 		a_output[["sd"]][aa,oo] <- sd(unlist(res_list["a_est",]))
@@ -102,23 +119,83 @@ for(aa in 1:length(ar)){
 }
 end <- Sys.time() - start
 
+saveRDS(a_output, "alpha_output.rds")
+saveRDS(proc_output, "process_output.rds")
+saveRDS(obs_output, "obs_output.rds")
+
+par(mfrow=c(3,3), mar=c(0,0,0,0), omi=c(1,1,1,1))
+plot(x=1:3, y=a_output$mean[1,], pch=16, cex=1.5, xlim=c(0,4), ylim=c(-1,1), xaxt="n", las=2, ylab="")
+segments(x0=1:3,x1=1:3,y0=a_output$lower[1,],y1=a_output$upper[1,], lwd=2)
+abline(h=ar[1], col="red")
+mtext(expression("True" ~ alpha ~ "= -0.5"), side=2, line=5, cex=1.5)
+mtext("Estimate", side=2, line=3)
+mtext(expression(alpha), side=3, line=2, font=2, cex=1.2)
+
+plot(x=1:3, y=proc_output$mean[1,], pch=16, cex=1.5, xlim=c(0,4), ylim=c(-1,1), xaxt="n", yaxt="n", las=2, ylab="")
+segments(x0=1:3,x1=1:3,y0=proc_output$lower[1,],y1=proc_output$upper[1,], lwd=2)
+abline(h=0.4, col="red")
+mtext(expression(sigma[p]^2), side=3, line=1, font=2, cex=1.2)
+
+plot(x=1:3, y=obs_output$mean[1,], pch=16, cex=1.5, xlim=c(0,4), ylim=c(-1,1), xaxt="n", yaxt="n", las=2, ylab="")
+segments(x0=1:3,x1=1:3,y0=obs_output$lower[1,],y1=obs_output$upper[1,], lwd=2)
+abline(h=obs[1], col="red")
+mtext(expression(sigma[y]^2), side=3, line=1, font=2, cex=1.2)
+
+plot(x=1:3, y=a_output$mean[2,], pch=16, cex=1.5, xlim=c(0,4), ylim=c(-1,1), xaxt="n", las=2, ylab="")
+segments(x0=1:3,x1=1:3,y0=a_output$lower[2,],y1=a_output$upper[2,], lwd=2)
+abline(h=ar[2], col="red")
+mtext(expression("True" ~ alpha ~ "= 0"), side=2, line=5, cex=1.5)
+mtext("Estimate", side=2, line=3)
+
+plot(x=1:3, y=proc_output$mean[2,], pch=16, cex=1.5, xlim=c(0,4), ylim=c(-1,1), xaxt="n", yaxt="n", las=2, ylab="")
+segments(x0=1:3,x1=1:3,y0=proc_output$lower[2,],y1=proc_output$upper[2,], lwd=2)
+abline(h=0.4, col="red")
+
+plot(x=1:3, y=obs_output$mean[2,], pch=16, cex=1.5, xlim=c(0,4), ylim=c(-1,1), xaxt="n", yaxt="n", las=2, ylab="")
+segments(x0=1:3,x1=1:3,y0=obs_output$lower[2,],y1=obs_output$upper[2,], lwd=2)
+abline(h=obs[2], col="red")
+
+plot(x=1:3, y=a_output$mean[3,], pch=16, cex=1.5, xlim=c(0,4), ylim=c(-1,1), xaxt="n", las=2, ylab="")
+segments(x0=1:3,x1=1:3,y0=a_output$lower[3,],y1=a_output$upper[3,], lwd=2)
+axis(1, at=1:3, labels=c(0.2, 0.4, 0.8))
+abline(h=ar[3], col="red")
+mtext(expression("True" ~ alpha ~ "= 0.5"), side=2, line=5, cex=1.5)
+mtext("Estimate", side=2, line=3)
+
+plot(x=1:3, y=proc_output$mean[3,], pch=16, cex=1.5, xlim=c(0,4), ylim=c(-1,1), xaxt="n", yaxt="n", las=2, ylab="")
+segments(x0=1:3,x1=1:3,y0=proc_output$lower[3,],y1=proc_output$upper[3,], lwd=2)
+axis(1, at=1:3, labels=c(0.2, 0.4, 0.8))
+abline(h=0.4, col="red")
+
+plot(x=1:3, y=obs_output$mean[3,], pch=16, cex=1.5, xlim=c(0,4), ylim=c(-1,1), xaxt="n", yaxt="n", las=2, ylab="")
+segments(x0=1:3,x1=1:3,y0=obs_output$lower[3,],y1=obs_output$upper[3,], lwd=2)
+axis(1, at=1:3, labels=c(0.2, 0.4, 0.8))
+abline(h=obs[3], col="red")
+
+mtext(side=1, "True Observation Error", line=3, outer=TRUE, cex=1.5)
+mtext(side=3, "Parameter", line=4, outer=TRUE, cex=1.5)
 
 
+###########################
+## gompertz model
+###########################
 
-plot(sim$ytrue, type = 'b', col = 2, pch = 's', ylim = c(min(sim$yobs), max(sim$yobs)))
-lines(sim$yobs)
-points(sim$yobs, pch = 'o')
+gsim <- gompertz_sim(N = 1000, seed=123, sigma.obs=0.2, sigma.proc=0.5, a=1.2, b=0.7, y1=0.5)
 
-  # extract estimated process:
-  u <- summary(sd, "random")[, "Estimate"]
-  u_se <- summary(sd, "random")[, "Std. Error"]
+par(mfrow=c(2,1), mar=c(1,1,1,1))
+plot(x=1, y=1, type="n", ylim=c(0, max(gsim$ytrue)), xlim=c(0,1000), xaxs="i", yaxs="i")
+polygon(x=c(0,900,900,0), y=c(0,0,7,7), col="gray")
+lines(gsim$ytrue, type="o", lwd=2, ylim = c(min(gsim$ytrue), max(gsim$ytrue)))
+points(gsim$yobs, col="blue", pch=19)
+lines(gsim$yobs, col="blue", lty=2)
 
-  plot(u, type="l", lwd=2)
-  polygon(x=c(1:length(u_se), length(u_se):1), y=c(u-u_se, rev(u)+rev(u_se)), col=rgb(1,0,0,0.5))
-  lines(sim$ytrue, lty=2, lwd=3)
-  
- 
-  
+gsim_cut <- lapply(1:length(gsim), function(x) gsim[[x]][901:1000])
+names(gsim_cut) <- c("yobs", "ytrue")
+plot(gsim_cut$ytrue, type="o", lwd=2, ylim=c(0, max(gsim$ytrue)))
+points(gsim_cut$yobs, col="blue", pch=19)
+lines(gsim_cut$yobs, col="blue", lty=2)
+legend("bottomleft", pch=c(1,19), col=c("black", "blue"), legend=c("state", "observation"), lwd=c(2,1), lty=c(1,2))
+mtext(side=1, "Observation #", line=3)
 
 
-  
+gres <- DLM_run(sim=gsim_cut, model=1)
--- Week 3 -- Temporal Models/Homework/dlm_hw3.cpp
+++ Week 3 -- Temporal Models/Homework/dlm_hw3.cpp
@@ -5,10 +5,12 @@
 template<class Type>
 Type objective_function<Type>::operator() () {
 // data:
+DATA_INTEGER(model);
 DATA_VECTOR(y);
 
 // parameters:
 PARAMETER(a); // population growth rate parameter
+PARAMETER(b);
 PARAMETER(log_sigma_proc); // log(process SD)
 PARAMETER(log_sigma_obs); // log(observation SD)
 PARAMETER_VECTOR(u); // unobserved state
@@ -27,10 +29,13 @@ int n = y.size(); // get time series length
 Type nll = 0.0; // initialize negative log likelihood
 
 // process model:
+vector<Type> expected(n);
+expected(0) = u(0);
 for(int i = 1; i < n; i++){
 	//linear model
-    Type m = a*u[i-1] ;    // calculated mean of the process, m keeping track of predicted value, state at time t-1, recast every time the loop goes through 
-  nll -= dnorm(u[i], m, sigma_proc, true); //likelihood for random effects
+  if(model==0) expected(i) = a*u[i-1] ;    // calculated mean of the process, 'expected' keeping track of predicted value, state at time t-1, recast every time the loop goes through 
+  if(model==1) expected(i) = a + b*u[i-1];
+  nll -= dnorm(u[i], expected(i), sigma_proc, true); //likelihood for random effects
 }
 
 // observation model:
